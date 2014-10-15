d80be70a9552f05b657935968b78a5687f9f7864
Prathmesh and Rajshekar <devnull@localhost>
2014-10-16 01:24:56 +0530

    Bugfix

--- src/simulator/generic/Port.java
+++ src/simulator/generic/Port.java
@@ -61,7 +61,7 @@ public class Port
 		if(this.portType == PortType.Unlimited)
 		{
 			// For an unlimited port, set the event with current-time.
-			event.setEventTime(GlobalClock.getCurrentTime() + latencyOfConnectedElement);
+			event.addEventTime(GlobalClock.getCurrentTime() + latencyOfConnectedElement);
 			event.getEventQ().addEvent(event);
 			return;
 		}
@@ -95,7 +95,7 @@ public class Port
 			}
 						
 			// set the time of the event
-			event.setEventTime(portBusyUntil[availablePortID] + latencyOfConnectedElement);
+			event.addEventTime(portBusyUntil[availablePortID] + latencyOfConnectedElement);
 			
 			// add event in the eventQueue
 			event.getEventQ().addEvent(event);
