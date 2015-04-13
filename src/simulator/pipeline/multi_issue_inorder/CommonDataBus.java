180fc5dedb75942e6d503d2b718888708f7ccf84
Abhishek Kumar <abhishek.iitd16@gmail.com>
2015-04-13 23:39:24 +0530

    Common Data bus functions added.

---
+++ src/simulator/pipeline/multi_issue_inorder/CommonDataBus.java
@@ -0,0 +1,75 @@
+package pipeline.multi_issue_inorder;
+
+public class CommonDataBus {
+	int size;
+	boolean busy[];
+	int register[];
+	Object value[];
+	int occupied;
+	
+	public CommonDataBus(int size) {
+		this.size = size;
+		this.busy = new boolean[size];
+		this.register = new int[size];
+		this.value = new Object[size];
+		this.occupied = 0;
+		for (int i = 0; i < size; i++) {
+			this.busy[i] = false;
+		}
+	}
+	
+	public int find(int register){
+		for (int i=0; i<size; i++){
+			if (this.register[i]==register){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void insert(int register, Object value){
+		// Not sure if this register is already there
+		int r = find(register);
+		if (r==-1){
+			for (int i = 0; i < size; i++) {
+				if (!this.busy[i]) {
+					this.register[i] = register;
+					this.value[i] = value;
+					this.busy[i] = true;
+				}
+			}
+		}else{
+			this.value[r] = value;
+			if (this.busy[r]){
+				System.out.println("Something might be wrong. Overwriting register "+r+" in CDB.");
+			}else{
+				occupied++;
+			}
+			this.busy[r] = true;
+		}
+	}
+	
+	public Object get(int register){
+		int r = find(register);
+		if (r==-1){
+			return null;
+		}else{
+			if (!busy[r]){
+				System.out.println("Something might be wrong. Reading register "+r+" again.");
+			}else{
+				occupied--;
+			}
+			busy[r] = false;
+			return value[r];
+		}
+	}
+	
+	public boolean isFull(){
+		return occupied==size;
+	}
+	
+	public static int getCDBSize(){
+		return 4;
+	}
+	
+}
