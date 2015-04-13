18d97cb6ab63d8fad3362b1a690f00950bdc4a9c
Akhil Jain <akhil.jain93@gmail.com>
2015-04-13 21:14:34 +0530

    multiple changes

---
+++ src/simulator/pipeline/multi_issue_inorder/RF.java
@@ -0,0 +1,22 @@
+package pipeline.multi_issue_inorder;
+
+import generic.Operand;
+
+class Register {
+	int busy;
+	Operand value;
+	int Qi;
+
+}
+
+public class RF {
+	int size;
+	Register rf[];
+
+	RF(int size) {
+		this.size = size;
+		rf = new Register[size];
+		for (int i = 0; i < size; i++)
+			rf[i] = new Register();
+	}
+}
