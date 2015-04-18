29330a7e5336d15fb8f143f8560a6f765b855e56
Akhil Jain <akhil.jain93@gmail.com>
2015-04-18 14:10:41 +0530

    a few changes in CDB

--- src/simulator/pipeline/multi_issue_inorder/CommonDataBus.java
+++ src/simulator/pipeline/multi_issue_inorder/CommonDataBus.java
@@ -5,15 +5,16 @@ import generic.EventQueue;
 import generic.PortType;
 import generic.SimulationElement;
 
-public class CommonDataBus extends SimulationElement{
+public class CommonDataBus extends SimulationElement {
 	int size;
 	boolean busy[];
 	int register[];
 	Object value[];
 	int occupied;
-	
+
 	public CommonDataBus(int size) {
-		super(PortType.FirstComeFirstServe, CommonDataBus.getCDBSize(), 1, 0, -1);
+		super(PortType.FirstComeFirstServe, CommonDataBus.getCDBSize(), 1, 0,
+				-1);
 		this.size = size;
 		this.busy = new boolean[size];
 		this.register = new int[size];
@@ -23,20 +24,20 @@ public class CommonDataBus extends SimulationElement{
 			this.busy[i] = false;
 		}
 	}
-	
-	public int find(int register){
-		for (int i=0; i<size; i++){
-			if (this.register[i]==register){
+
+	public int find(int register) {
+		for (int i = 0; i < size; i++) {
+			if (this.register[i] == register) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
-	public boolean insert(int register, Object value){
+
+	public boolean insert(int register, Object value) {
 		// Not sure if this register is already there
 		int r = find(register);
-		if (r==-1){
+		if (r == -1) {
 			for (int i = 0; i < size; i++) {
 				if (!this.busy[i]) {
 					this.register[i] = register;
@@ -45,11 +46,13 @@ public class CommonDataBus extends SimulationElement{
 					return true;
 				}
 			}
-		}else{
+		} else {
 			this.value[r] = value;
-			if (this.busy[r]){
-				System.out.println("Something might be wrong. Overwriting register "+r+" in CDB.");
-			}else{
+			if (this.busy[r]) {
+				System.out
+						.println("Something might be wrong. Overwriting register "
+								+ r + " in CDB.");
+			} else {
 				occupied++;
 			}
 			this.busy[r] = true;
@@ -57,34 +60,36 @@ public class CommonDataBus extends SimulationElement{
 		}
 		return false;
 	}
-	
-	public Object get(int register){
+
+	public Object get(int register) {
 		int r = find(register);
-		if (r==-1){
+		if (r == -1) {
 			return null;
-		}else{
-			if (!busy[r]){
-				System.out.println("Something might be wrong. Reading register "+r+" again.");
-			}else{
+		} else {
+			if (!busy[r]) {
+				System.out
+						.println("Something might be wrong. Reading register "
+								+ r + " again.");
+			} else {
 				occupied--;
 			}
 			busy[r] = false;
 			return value[r];
 		}
 	}
-	
-	public boolean isFull(){
-		return occupied==size;
+
+	public boolean isFull() {
+		return occupied == size;
 	}
-	
-	public static int getCDBSize(){
+
+	public static int getCDBSize() {
 		return 4;
 	}
 
 	@Override
 	public void handleEvent(EventQueue eventQ, Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
